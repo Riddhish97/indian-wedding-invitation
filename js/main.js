a65a2daa03c82cacd49ceedfb54eed50952cdfc9
Riddhish <riddhishraj@gmail.com>
2024-02-03 14:20:01 +0530

    Dynamic name added on loading page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-var */
-;(function ($) {
-  'use strict'
+(function ($) {
+  "use strict";
 
   // Navbar on scrolling
   // $(window).scroll(function () {
@@ -12,70 +12,66 @@
   // })
 
   // Smooth scrolling on the navbar links
-  $('.navbar-nav a').on('click', function (event) {
-    if (this.hash !== '') {
-      event.preventDefault()
+  $(".navbar-nav a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
 
-      $('html, body').animate(
+      $("html, body").animate(
         {
           scrollTop: $(this.hash).offset().top - 45,
         },
         1500,
-        'easeInOutExpo'
-      )
+        "easeInOutExpo"
+      );
 
-      if ($(this).parents('.navbar-nav').length) {
-        $('.navbar-nav .active').removeClass('active')
-        $(this).closest('a').addClass('active')
+      if ($(this).parents(".navbar-nav").length) {
+        $(".navbar-nav .active").removeClass("active");
+        $(this).closest("a").addClass("active");
       }
     }
-  })
+  });
 
   $(document).ready(function () {
-    var full = window.location.host
-    // window.location.host is subdomain.domain.com
-    var parts = full.split('.')
-    var subdomain = parts[0]
-    var guestName = subdomain
-      .split('-')
-      .map(function (element) {
-        return element.charAt(0).toUpperCase() + element.slice(1)
-      })
-      .join(' ')
-    // document.getElementById('guest-name').textContent = guestName
-    // document.getElementById('guest-name-main-page').textContent = guestName
+    // Function to extract parameter value from URL
+    function getParameterValue(url, parameter) {
+      const urlSearchParams = new URLSearchParams(new URL(url).search);
+      return urlSearchParams.get(parameter);
+    }
+    let guestName = getParameterValue(window.location.href, "name");
+    document.getElementById("guest-name").textContent = guestName;
+    document.getElementById("guest-name-main-page").textContent = guestName;
 
     // sliding invite cover
-    var timeleft = 5
+    var timeleft = 5;
     var downloadTimer = setInterval(function () {
-      timeleft--
-      var countdown = document.getElementById('countdown')
+      timeleft--;
+      var countdown = document.getElementById("countdown");
       if (countdown) {
-        countdown.textContent = timeleft
+        countdown.textContent = timeleft;
       }
-      if (timeleft <= 0) clearInterval(downloadTimer)
-    }, 1000)
+      if (timeleft <= 0) clearInterval(downloadTimer);
+    }, 1000);
 
     setInterval(function () {
-      const fade = { opacity: 0, transition: 'opacity 400ms', zIndex: -1 }
-      $('#invite-cover').css(fade).slideUp()
-    }, 5000)
+      const fade = { opacity: 0, transition: "opacity 400ms", zIndex: -1 };
+      $("#invite-cover").css(fade).slideUp();
+    }, 5000);
 
     // venue map
-    $('#venue-map').click(function () {
+    $("#venue-map").click(function () {
       //   $('.navigate-link').click()
       window.open(
-        'https://maps.google.com/maps/dir//Quality+Inn+Palms+Survey+No.98,+Near,+Kandla+Airport,+ANJAR+,+MeghparBorichi+Anjar,+Gujarat+370110/@23.097435,70.098716,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0x3950b91d6fff515b:0x98411f3bce9b0c84',
-        '_blank'
-      )
-    })
+        "https://maps.google.com/maps/dir//Quality+Inn+Palms+Survey+No.98,+Near,+Kandla+Airport,+ANJAR+,+MeghparBorichi+Anjar,+Gujarat+370110/@23.097435,70.098716,16z/data=!4m5!4m4!1m0!1m2!1m1!1s0x3950b91d6fff515b:0x98411f3bce9b0c84",
+        "_blank"
+      );
+    });
 
     // Modal Video
-    var $videoSrc
-    $('.btn-play').click(function () {
-      $videoSrc = $(this).data('src')
-    })
-    console.log($videoSrc)
+    var $videoSrc;
+    $(".btn-play").click(function () {
+      $videoSrc = $(this).data("src");
+    });
+    console.log($videoSrc);
 
     // Define the carousel settings on the window object
     window.galleryCarouselSettings = {
@@ -95,61 +91,61 @@
         992: { items: 4 },
         1200: { items: 5 },
       },
-    }
+    };
 
     // Function to initialize the carousel
     window.initGallery = function () {
-      $('.gallery-carousel').owlCarousel(window.galleryCarouselSettings)
-    }
+      $(".gallery-carousel").owlCarousel(window.galleryCarouselSettings);
+    };
 
     // Initial call to setup the carousel
-    window.initGallery()
+    window.initGallery();
 
-    $('#videoModal').on('shown.bs.modal', function (e) {
-      $('#video').attr(
-        'src',
-        $videoSrc + '?autoplay=1&amp;modestbranding=1&amp;showinfo=0'
-      )
-    })
+    $("#videoModal").on("shown.bs.modal", function (e) {
+      $("#video").attr(
+        "src",
+        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
+      );
+    });
 
-    $('#videoModal').on('hide.bs.modal', function (e) {
-      $('#video').attr('src', $videoSrc)
-    })
-  })
+    $("#videoModal").on("hide.bs.modal", function (e) {
+      $("#video").attr("src", $videoSrc);
+    });
+  });
 
   // Scroll to Bottom
   $(window).scroll(function () {
     if ($(this).scrollTop() > 100) {
-      $('.scroll-to-bottom').fadeOut('slow')
+      $(".scroll-to-bottom").fadeOut("slow");
     } else {
-      $('.scroll-to-bottom').fadeIn('slow')
+      $(".scroll-to-bottom").fadeIn("slow");
     }
-  })
+  });
 
   // Portfolio isotope and filter
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-item',
-    layoutMode: 'fitRows',
-  })
-  $('#portfolio-flters li').on('click', function () {
-    $('#portfolio-flters li').removeClass('active')
-    $(this).addClass('active')
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') })
-  })
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
 
   // Back to top button
   $(window).scroll(function () {
     if ($(this).scrollTop() > 200) {
-      $('.back-to-top').fadeIn('slow')
+      $(".back-to-top").fadeIn("slow");
     } else {
-      $('.back-to-top').fadeOut('slow')
+      $(".back-to-top").fadeOut("slow");
     }
-  })
-  $('.back-to-top').click(function () {
-    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo')
-    return false
-  })
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
   // Gallery carousel
-})(jQuery)
+})(jQuery);
